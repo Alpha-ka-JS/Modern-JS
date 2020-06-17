7897ef271fc537b1b7058ec55a8afa71ef5b250c
jehwan <wpghks7@naver.com>
2020-06-18 02:45:34 +0900

    apply prettier formatter

--- LeeJehwan/ch2/factorial.js
+++ LeeJehwan/ch2/factorial.js
@@ -1,7 +1,7 @@
 function fact(n) {
-    if( n<= 1 ) return n;
-    return n*fact(n-1);
+  if (n <= 1) return n;
+  return n * fact(n - 1);
 }
-for(var i = 1; i < 10; i++) {
-    console.log(i + "!= " + fact(i));
+for (var i = 1; i < 10; i++) {
+  console.log(i + "!= " + fact(i));
 }
