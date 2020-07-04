513900f22f4a45159147270468f79f1fde59e162
jehwan <wpghks7@naver.com>
2020-07-05 00:07:56 +0900

    add 7.2 조건문

---
+++ LeeJehwan/ch7/7.2.1.js
@@ -0,0 +1,21 @@
+var name = undefined;
+if (!name) {
+  name = "";
+  console.log("이름을 입력하십시오");
+}
+var x = 10;
+if (x >= 0) {
+  console.log("positive or zero");
+} else {
+  console.log("Negative");
+}
+
+function isLeapYear(year) {
+  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isLeapYear(100));
+console.log(isLeapYear(2020));
