d1e0560eb15d4ed8adf45a8d3637436412e7b580
성원 정 <chsw93@gmail.com>
2020-07-13 17:13:52 +0900

    [Chapter8] 8.9 code

--- JeongSungWon/ch8/ch8_8.js
+++ JeongSungWon/ch8/ch8_8.js
@@ -25,7 +25,6 @@ function fibonacci(n){
     }
     return fibonacci[n];
 }
-// for(var i=0; i<=20; i++){
-//     console.log((" "+i).slice(-2)+":"+fibonacci(i));
-// }
-console.log(fibonacci[3]);
+for(var i=0; i<=20; i++){
+    console.log((" "+i).slice(-2)+":"+fibonacci(i));
+}
