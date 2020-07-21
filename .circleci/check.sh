55999b61e813a0cd4c8117435cc182927f577b66
jehwan <wpghks7@naver.com>
2020-07-22 01:06:20 +0900

    test

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USER_NAME=$0
-echo USER_NAME is $0
-USER_NAME_ARG=$1
+USER_NAME=$1
+echo USER_NAME is $1
+USER_NAME_ARG=$2
 
 if [ $USER_NAME_ARG != $USER_NAME ]; then
     echo $USER_NAME_ARG is not $USER_NAME
