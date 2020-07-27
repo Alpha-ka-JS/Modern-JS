a229755af3c77d562e5a738b3e2b968a5cba9592
jehwan <wpghks7@naver.com>
2020-07-27 18:14:33 +0900

    1

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -4,7 +4,7 @@ COMMIT=${CIRCLE_SHA1}
 echo commit log is ${COMMIT}
 GIT_LOG=$(git log -1 --pretty=format:"%H" -- ./$1)
 echo git log is ${GIT_LOG}
-
+echo hi
 if [ "${COMMIT}" != "${GIT_LOG}" ]; then
     echo cancel circle ci
     circleci-agent step halt
