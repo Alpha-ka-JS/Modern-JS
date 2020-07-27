19df10b58f7a2b3f921f319412abf7a99cb83d2e
jehwan <wpghks7@naver.com>
2020-07-27 17:50:36 +0900

    test

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 COMMIT=${CIRCLE_SHA1}
-echo ${COMMIT}
-GIT_LOG=$(git log -1 --format=format%H -- ./${$0})
-echo test ${GIT_LOG}
+echo commit log is ${COMMIT}
+GIT_LOG=$(git log -1 --format=format%H -- ./$0)
+echo git log is ${GIT_LOG}
 
 if [ "${COMMIT}" != "${GIT_LOG}" ]; then
     circleci-agent step halt
