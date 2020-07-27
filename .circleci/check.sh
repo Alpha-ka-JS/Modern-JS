ef63cd170a8c370686a4a693523b9409b96d10f6
jehwan <wpghks7@naver.com>
2020-07-27 17:57:42 +0900

    test2

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -2,7 +2,7 @@
 
 COMMIT=${CIRCLE_SHA1}
 echo commit log is ${COMMIT}
-GIT_LOG=$(git log -1 --format=format%H -- ./$0)
+GIT_LOG=$(git log -1 --pretty=format:"%H" -- ./$0)
 echo git log is ${GIT_LOG}
 
 if [ "${COMMIT}" != "${GIT_LOG}" ]; then
