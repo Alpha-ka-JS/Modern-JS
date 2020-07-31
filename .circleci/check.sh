abfa5f615d46ee7243cb22bb8afe7f64228aa0cd
Jehwan <43315929+LeeJehwan@users.noreply.github.com>
2020-08-01 03:09:59 +0900

    fix typo

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -6,6 +6,6 @@ GIT_LOG=$(git log -1 --pretty=format:"%H" -- ./$1)
 echo git log is ${GIT_LOG}
 
 if [ "${COMMIT}" != "${GIT_LOG}" ]; then
-    echo cancle circle ci
+    echo cancel circle ci
     circleci-agent step halt
 fi
