bdfcb28e5ba61ab55a722f556c09a2fedde29e51
sungwon2054 <52375252+sungwon2054@users.noreply.github.com>
2020-09-20 20:56:26 +0900

    [Chapter15] 15.8 비동기 처리를 간결하게 작성하는 Promise

--- JeongSungWon/ch15/ch15_8.js
+++ JeongSungWon/ch15/ch15_8.js
@@ -77,3 +77,94 @@ promise.then(
     console.log(error);
   }
 );
+
+//15.8.6 Promise가 실행하는 콜백 함수에 인수 넘기기
+function buyAsync(mymoney) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      var payment = parseInt(promt('지불하고자 하는 금액을 입력하십시오'));
+      var balance = mymoney - payment;
+      if (balance > 0) {
+        console.log(`${payment}원을 지불했습니다. `);
+        resolve(balance);
+      } else {
+        reject(`잔액은 ${mymoney}원입니다. 구매할 수 없습니다`);
+      }
+    }, 1000);
+  });
+}
+buyAsync(500)
+  .then(function (balance) {
+    console.log(`잔액은 ${balance}원입니다.`);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+//15.8.7 Promise로 비동기 처리 연결하기
+function buyAsync(mymoney) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      var payment = parseInt(prompt('지불하고자 하는 금액을 입력하십시오'));
+      var balance = mymoney - payment;
+      if (balance > 0) {
+        console.log(`${payment}원을 지불했습니다.`);
+        resolve(balance);
+      } else {
+        reject(`잔액은 ${mymoney}원 입니다. 구매할 수 없습니다`);
+      }
+    }, 1000);
+  });
+}
+buyAsync(500)
+  .then(function (balance) {
+    console.log(`잔액은 ${balance}원입니다.`);
+    return buyAsync(balance);
+  })
+  .then(function (balance) {
+    console.log(`잔액은 ${balance}원입니다.`);
+    return buyAsync(balance);
+  })
+  .then(function (balance) {
+    console.log(`잔액은 ${balance}원입니다.`);
+    return buyAsync(balance);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+//15.8.8 비동기 처리 여러 개를 병렬로 실행하기
+function buyAsync(name, mymoney) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      var payment = parseInt(
+        prompt('${name}님, 지불하고자 하는 금액을 입력하십시오')
+      );
+      var balance = mymoney - payment;
+      if (balance > 0) {
+        console.log(`${name} : ${payment}원을 지불했습니다.`);
+        resolve(balance);
+      } else {
+        reject(`${name} : 잔액은 ${balance}}원 입니다. 구매할 수 없습니다`);
+      }
+    }, 1000);
+  });
+}
+/*
+promise
+  .all([buyAsync('Tom', 500), buyAsync('Huck', 600), buyAsync('Becky', 1000)])
+  .then(function (balance) {
+    console.log(balance);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+*/
+promise
+  .race([buyAsync('Tom', 500), buyAsync('Huck', 600), buyAsync('Becky', 1000)])
+  .then(function (balance) {
+    console.log(balance);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
