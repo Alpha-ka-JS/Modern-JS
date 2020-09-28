337a06092c49412870eb32aef145a14b8a8e99dc
Jehwan <43315929+LeeJehwan@users.noreply.github.com>
2020-09-29 04:29:40 +0900

    Update check.sh

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-COMMIT=${CIRCLE_SHA1}
-echo commit log is ${COMMIT}
-GIT_LOG=$(git log -1 --pretty=format:"%H" -- ./$1)
+GIT_LOG=$(git log origin/master.. --oneline --format="%H" ./$1)
 echo git log is ${GIT_LOG}
 
-if [ "${COMMIT}" != "${GIT_LOG}" ]; then
+if [ -z "${GIT_LOG}" ]; then
     echo cancel circle ci
     circleci-agent step halt
 fi
